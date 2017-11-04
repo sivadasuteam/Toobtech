5c3e2bd7a5234abbafadadd95d13efb44b899604
yunus <yunus@YUNUS-PC>
2017-11-04 14:05:24 +0530

    calucaltion

--- TT/First/First/BasicPrograms_yunus.cs
+++ TT/First/First/BasicPrograms_yunus.cs
@@ -7,30 +7,47 @@ namespace First
 {
     public class BasicPrograms_yunus
     {
+        public static void Main(string[] args)
+        {
+            BasicPrograms_yunus calucalation = new BasicPrograms_yunus();
+            calucalation.sub();
+         //   calucalation.Mul();
+           // calucalation.Name("shaik ", "yunus");
+            //calucalation.Div(8, 6);
+            //calucalation.Add(4, 9);
+            Console.ReadLine();
 
-        public void sub()
+
+        }
+        public void sub()//without return type----without parameters
         {
             int a = 10, b = 5;
             int result = a - b;
 
             Console.WriteLine(result);
         }
-        public int Mul()
+        public int Mul()//with return type----without parameters 
         {
-            int a = 10, b = 5;
-            int result = a - b;
+            int a = 10, b = 5; 
+            int result = a * b;
             return result;
         }
-        public string Name(string surname, string firstname)
+        public string Name(string surname, string firstname)//with return type----with parameters 
         {
             return surname + firstname;
 
         }
-        public int Mul(int a, int b)
+        public int Div(int a, int b)//with return type----with parameters 
         {
 
-            int result = a - b;
+            int result = a / b;
             return result;
+        }
+        public void Add(int a, int b)//without return type----with parameters 
+        {
+
+            int result = a + b;
+            Console.WriteLine(result);
         }
     }
 }
