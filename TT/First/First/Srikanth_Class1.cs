4493c8fc6dc2d79e914623d3483f87c2c5fdd5fb
Ayaan <Pulla.site@outlook.com>
2017-11-04 07:30:34 +0530

    Class Program added

---
+++ TT/First/First/Srikanth_Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace First
+{
+    public class Srikanth_Class1
+    {
+        int b;
+        int i;
+        int res = i + b;
+
+
+    }
+}
