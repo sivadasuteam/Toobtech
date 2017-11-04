412bb72cf5a1a32a8b4dd30c9f6e33ce0f62c87b
SURAJ <SURAJ@SURAJ-PC>
2017-11-04 07:51:50 +0530

    adding  method

---
+++ TT/First/First/BasicPrograms_Prasanna.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace First
+{
+    public class BasicPrograms_Prasanna
+    {
+        public void Even()
+        {
+            for (int num = 2; num <= 1000; num++)
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine(num);
+                }
+            Console.ReadLine();
+        }
+
+
+    }
+}
