5821174399793e5645a94edb8cc02319dbe1af0c
Ayaan <Pulla.site@outlook.com>
2017-11-11 07:43:01 +0530

    web project added

---
+++ TT/First/Web/Controllers/SrikanthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class SrikanthController : Controller
+    {
+        // GET: Srikanth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult New()
+        {
+            return View();
+        }
+    }
+}
