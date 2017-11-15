68fcd10f9b7d4526e135dc480df155ae024c9700
Srikanth <Srikanth@SRIKANTH-PC>
2017-11-15 08:12:46 +0530

    jquery added

---
+++ TT/First/Web/Controllers/User_sai.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Web1.Controllers
+{
+    internal class User_sai
+    {
+        public User_sai()
+        {
+        }
+
+        internal bool IsValid(string uid, string pwd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
